2187c24fbbb928ec51360e05efd53d3d5fc4861d
Jennifer Liu <jnnfrliu33@gmail.com>
2017-10-30 22:28:23 +0800

    Set up map view controller

--- OnTheMap/Controller/MapViewController.swift
+++ OnTheMap/Controller/MapViewController.swift
@@ -9,18 +9,86 @@
 import UIKit
 import MapKit
 
-// MARK: - MapViewController: UIViewController, MKMapViewDelegate
+// MARK: - MapViewController: UIViewController
 
-class MapViewController: UIViewController, MKMapViewDelegate {
+class MapViewController: UIViewController {
 
     // MARK: Outlets
     
     @IBOutlet weak var mapView: MKMapView!
-    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    // MARK: Properties
+    
+    var studentLocations = [StudentInformation]()
+    
+    // MARK: Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Create an MKPointAnnotation for each dictionary in "studentLocations"
+        var annotations = [MKPointAnnotation]()
+        
+        ParseClient.sharedInstance().getStudentLocations() { (studentLocations, error) in
+            if let studentLocations = studentLocations {
+                self.studentLocations = studentLocations as! [StudentInformation]
+                
+                for studentLocation in self.studentLocations {
+                    let latitude = CLLocationDegrees(studentLocation.latitude!)
+                    let longitude = CLLocationDegrees(studentLocation.longitude!)
+                    let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+                    
+                    let firstName = studentLocation.firstName!
+                    let lastName = studentLocation.lastName!
+                    let mediaURL = studentLocation.mediaURL!
+                    
+                    let annotation = MKPointAnnotation()
+                    annotation.coordinate = coordinate
+                    annotation.title = firstName + " " + lastName
+                    annotation.subtitle = mediaURL
+                    
+                    annotations.append(annotation)
+                }
+            } else {
+                print (error ?? "empty error")
+            }
+            
+            self.mapView.addAnnotations(annotations)
+        }
     }
+}
 
+// MARK: - MapViewController: MKMapViewDelegate
 
+extension MapViewController: MKMapViewDelegate {
+    
+    // Create a view with a "right callout accessory view"
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        let reuseId = "PinIcon"
+        
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+        
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView!.pinTintColor = .red
+            pinView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        } else {
+            pinView!.annotation = annotation
+        }
+        
+        return pinView
+    }
+    
+    // This delegate method is implemented to respond to taps. It opens the system browser
+    // to the URL specified in the annotationViews subtitle property.
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if control == view.rightCalloutAccessoryView {
+            let app = UIApplication.shared
+            if let toOpen = view.annotation?.subtitle! {
+                app.open(URL(string: toOpen)!, options: [:], completionHandler: nil)
+            }
+        }
+    }
 }
